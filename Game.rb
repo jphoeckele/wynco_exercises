b3d8d724d7a7cd4098d787637682afcab81bcb80
jphoeckele <jphoeckele@gmail.com>
2015-10-06 18:26:21 -0400

    Game.rb

---
+++ Game.rb
@@ -0,0 +1,388 @@
+bad_choice = "██████╗  █████╗ ██████╗      ██████╗██╗  ██╗ ██████╗ ██╗ ██████╗███████╗██╗
+██╔══██╗██╔══██╗██╔══██╗    ██╔════╝██║  ██║██╔═══██╗██║██╔════╝██╔════╝██║
+██████╔╝███████║██║  ██║    ██║     ███████║██║   ██║██║██║     █████╗  ██║
+██╔══██╗██╔══██║██║  ██║    ██║     ██╔══██║██║   ██║██║██║     ██╔══╝  ╚═╝
+██████╔╝██║  ██║██████╔╝    ╚██████╗██║  ██║╚██████╔╝██║╚██████╗███████╗██╗
+╚═════╝ ╚═╝  ╚═╝╚═════╝      ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝ ╚═════╝╚══════╝╚═╝
+                                                                           "
+
+super_wyn = "███████╗██╗   ██╗██████╗ ███████╗██████╗     ██╗    ██╗██╗   ██╗███╗   ██╗    ██╗    ██╗ ██████╗ ██████╗ ██╗     ██████╗ 
+██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗    ██║    ██║╚██╗ ██╔╝████╗  ██║    ██║    ██║██╔═══██╗██╔══██╗██║     ██╔══██╗
+███████╗██║   ██║██████╔╝█████╗  ██████╔╝    ██║ █╗ ██║ ╚████╔╝ ██╔██╗ ██║    ██║ █╗ ██║██║   ██║██████╔╝██║     ██║  ██║
+╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗    ██║███╗██║  ╚██╔╝  ██║╚██╗██║    ██║███╗██║██║   ██║██╔══██╗██║     ██║  ██║
+███████║╚██████╔╝██║     ███████╗██║  ██║    ╚███╔███╔╝   ██║   ██║ ╚████║    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝
+╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝    ╚═╝   ╚═╝  ╚═══╝     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ 
+                                                                                                                          bash edition!"
+
+wynwood = "██╗    ██╗██╗   ██╗███╗   ██╗██╗    ██╗ ██████╗  ██████╗ ██████╗      ██╗               ██╗
+██║    ██║╚██╗ ██╔╝████╗  ██║██║    ██║██╔═══██╗██╔═══██╗██╔══██╗    ███║              ███║
+██║ █╗ ██║ ╚████╔╝ ██╔██╗ ██║██║ █╗ ██║██║   ██║██║   ██║██║  ██║    ╚██║    █████╗    ╚██║
+██║███╗██║  ╚██╔╝  ██║╚██╗██║██║███╗██║██║   ██║██║   ██║██║  ██║     ██║    ╚════╝     ██║
+╚███╔███╔╝   ██║   ██║ ╚████║╚███╔███╔╝╚██████╔╝╚██████╔╝██████╔╝     ██║               ██║
+ ╚══╝╚══╝    ╚═╝   ╚═╝  ╚═══╝ ╚══╝╚══╝  ╚═════╝  ╚═════╝ ╚═════╝      ╚═╝               ╚═╝
+                                                                                           "
+
+good_call = " ██████╗  ██████╗  ██████╗ ██████╗      ██████╗ █████╗ ██╗     ██╗     ██╗
+██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗    ██╔════╝██╔══██╗██║     ██║     ██║
+██║  ███╗██║   ██║██║   ██║██║  ██║    ██║     ███████║██║     ██║     ██║
+██║   ██║██║   ██║██║   ██║██║  ██║    ██║     ██╔══██║██║     ██║     ╚═╝
+╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝    ╚██████╗██║  ██║███████╗███████╗██╗
+ ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝      ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝
+                                                                          "
+not_bad = "███╗   ██╗ ██████╗ ████████╗    ██████╗  █████╗ ██████╗          
+████╗  ██║██╔═══██╗╚══██╔══╝    ██╔══██╗██╔══██╗██╔══██╗         
+██╔██╗ ██║██║   ██║   ██║       ██████╔╝███████║██║  ██║         
+██║╚██╗██║██║   ██║   ██║       ██╔══██╗██╔══██║██║  ██║         
+██║ ╚████║╚██████╔╝   ██║       ██████╔╝██║  ██║██████╔╝██╗██╗██╗
+╚═╝  ╚═══╝ ╚═════╝    ╚═╝       ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝╚═╝
+                                                                 "
+
+def sleep_skip_game_over
+  sleep(4)
+  puts ("\n"* 12)
+  puts " ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ ██╗
+██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗██║
+██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝██║
+██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗╚═╝
+╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║██╗
+ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═╝
+                                                                             "
+end
+
+def sleep_skip_continued
+  sleep(4.5)
+  puts ("\n"* 12)
+  puts "████████╗ ██████╗     ██████╗ ███████╗     ██████╗ ██████╗ ███╗   ██╗████████╗██╗███╗   ██╗██╗   ██╗███████╗██████╗          
+╚══██╔══╝██╔═══██╗    ██╔══██╗██╔════╝    ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██║████╗  ██║██║   ██║██╔════╝██╔══██╗         
+   ██║   ██║   ██║    ██████╔╝█████╗      ██║     ██║   ██║██╔██╗ ██║   ██║   ██║██╔██╗ ██║██║   ██║█████╗  ██║  ██║         
+   ██║   ██║   ██║    ██╔══██╗██╔══╝      ██║     ██║   ██║██║╚██╗██║   ██║   ██║██║╚██╗██║██║   ██║██╔══╝  ██║  ██║         
+   ██║   ╚██████╔╝    ██████╔╝███████╗    ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║██║ ╚████║╚██████╔╝███████╗██████╔╝██╗██╗██╗
+   ╚═╝    ╚═════╝     ╚═════╝ ╚══════╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═════╝ ╚═╝╚═╝╚═╝
+                                                                                                                             "
+end
+
+def end_option
+    puts "Would you like to play again? Y or N"
+    replay = gets.chomp.upcase
+
+    if replay == "Y"
+        load"~/Desktop/Game.rb"
+    else
+        exit
+    end
+end
+
+def sleep_n_skip
+  sleep(2)
+  puts ("\n"*3)
+end
+
+def say(message)
+  puts message
+end
+
+def clear_page
+  puts ("\n"* 48)
+end
+
+
+
+
+# GAME BELOW
+
+clear_page
+say super_wyn
+sleep_n_skip        #Intro Story
+say "In typical sexist video game fashion,".upcase
+sleep_n_skip
+say "a helpless princess wearing a pink dress and tiara has been kidnapped and taken to a castle!".upcase
+sleep_n_skip
+say "You're a short, fat plumber with no combat experience. But you're a man!".upcase
+sleep_n_skip
+say "Which apparently in video games makes you a hero!".upcase
+sleep_n_skip
+say "You're the princess' only hope!".upcase
+sleep_n_skip
+sleep(5)
+clear_page
+say wynwood                                                                                     
+sleep_n_skip
+say "You begin your journey on a one way brick road.".upcase
+sleep_n_skip
+say "You see an unknown figure approaching and a castle in the distance".upcase
+sleep_n_skip
+say "There is nothing else around but sewer pipes and a few scattered blocks".upcase
+sleep_n_skip
+say "The castle seems like a good place to start.".upcase
+sleep_n_skip
+say "Do you: walk, run, do nothing?".upcase   #INITIAL CHOICE BETWEEN WALK, RUN, DO NOTHING
+option = gets.chomp.upcase
+ case option
+ when "walk".upcase   #WALK
+  	sleep_n_skip
+    clear_page
+    say not_bad
+    sleep_n_skip
+    say "However, at this rate this game is going to take forever!".upcase
+  	sleep_n_skip
+  	say "The unknown figure is only slightly closer.".upcase
+  	sleep_n_skip
+   	say "Now do you: walk, run, do nothing?".upcase
+    second_chance = gets.chomp.upcase
+    case second_chance
+    when "walk".upcase  #WALK, WALK
+    	sleep_n_skip
+      clear_page
+      say bad_choice
+      sleep_n_skip
+      say "You're lack of enthusiasm was impressive!".upcase
+    	sleep_n_skip
+    	say "Unfortunately because of your slow pace, terrible things have happened to the princess!".upcase
+    	sleep_n_skip
+    	say "Maybe we should have chosen your taller, thinner, and more attractive sibling?".upcase
+    	sleep_skip_game_over
+      end_option
+
+ 		when "run".upcase  #WALK, RUN
+ 			sleep_n_skip
+      clear_page
+      say not_bad
+      sleep_n_skip
+      say "Someone's played this game before!".upcase
+ 			sleep_n_skip
+  		say "The unknown figure is much closer now".upcase
+  		sleep_n_skip
+  		say "You see clearly that the figure is a giant turtle".upcase
+  		sleep_n_skip
+  		say "Do you: jump on it, jump over it, stop and ask for directions?".upcase
+  		attack = gets.chomp.upcase
+    	case attack
+    	when "jump on it".upcase #WALK, RUN, JUMP ON IT
+    		sleep_n_skip
+    		clear_page
+        say bad_choice
+    		sleep_n_skip
+    		say "In the state of Florida, jumping on a sea turtle is a felony!".upcase
+    		sleep_n_skip
+    		say "You are arrested and placed in the county jail with a $2,000.00 bail!".upcase
+    		sleep_n_skip
+    		say "Picking on a turtle... you really are the worst kind of person!".upcase
+    		sleep_n_skip
+    		say "Your mother should have raised you better!".upcase
+        sleep_n_skip
+        say "It's her fault, not yours!".upcase
+        sleep_n_skip
+        sleep_n_skip
+        clear_page
+        sleep_skip_game_over       
+        end_option
+
+    	when "jump over it".upcase #WALK, RUN, JUMP OVER IT
+    		clear_page
+        sleep_n_skip
+        say good_call
+        sleep_n_skip 
+        say "Violence is never the answer!".upcase
+    		sleep_n_skip
+    		say "You were able to make it to the castle before the arbitrary 3 minute timer expires!".upcase
+    		sleep_n_skip
+    		say "Unfortunately for you, the princess has been taken to another castle!".upcase
+    		sleep_n_skip
+    		say "Seriously, you should have seen this coming!".upcase
+    		sleep_n_skip
+    		say "Next time consider just starting at World 8 and save yourself about 5 hours!".upcase
+    		sleep_n_skip
+        sleep_skip_continued
+        end_option
+
+    	when "stop and ask for directions".upcase  #WALK, RUN, STOP AND ASK FOR DIRECTIONS
+    		sleep_n_skip
+        clear_page
+        say bad_choice
+        sleep_n_skip
+        say "What were you thinking?".upcase
+    		sleep_n_skip
+    		say "The turtle bites your big toe and now you can no longer run!".upcase
+    		sleep_n_skip
+    		say "Because of your slow pace, awful and dehumanizing things have happened to the princess!".upcase
+    		sleep_n_skip
+    		say "Maybe we should have sent your taller, thinner, and slightly more attractive sibling? \n ".upcase
+        clear_page
+        sleep_skip_game_over
+        end_option
+
+      else  #WALK, RUN, ANYTHING ELSE
+      	sleep_n_skip
+        clear_page
+        say bad_choice
+        sleep_n_skip
+        say "#{attack}? That was not one of the options!".upcase
+        sleep_n_skip
+        say "Learn to spell!".upcase
+      	sleep_n_skip
+      	say "At some point bad grammar will cost you more than this game!".upcase
+      	sleep_n_skip
+    		say "This was our fault, we shouldn't have sent a plummer!".upcase
+    		sleep_n_skip
+    		say "The princess never had a chance!".upcase
+    		clear_page
+        sleep_skip_game_over
+        end_option
+      end
+
+    when "do nothing".upcase  #WALK, DO NOTHING
+    	sleep_n_skip
+      clear_page
+      say bad_choice
+      sleep_n_skip
+      say "Do you even want to save this princess?".upcase
+    	sleep_n_skip
+    	say "Maybe you didn't understand the urgency of the situation?".upcase
+    	sleep_n_skip
+    	say "Kidnappings don't usually end with flowers and rainbows?".upcase
+    	sleep_n_skip
+    	say "The princess was never heard from again!".upcase
+    	clear_page
+      sleep_skip_game_over
+      end_option
+
+    else  #WALK, ANTHING ELSE
+      sleep_n_skip
+      clear_page
+      say bad_choice
+      sleep_n_skip
+      say "#{second_chance}? Are you drunk?".upcase
+      sleep_n_skip
+      say "Good god man, learn to spell!".upcase
+     	sleep_n_skip
+     	say "At some point it will cost you more than this game!".upcase
+     	sleep_n_skip
+   		say "Maybe we should have sent your taller, thinner, and slightly more attractive sibling?".upcase    		
+   		sleep_n_skip
+   		say "The princess is doomed!".upcase
+   		sleep_skip_game_over
+      end_option
+    end
+
+ 	when "run".upcase #RUN
+ 		sleep_n_skip
+    clear_page
+    say not_bad
+    sleep_n_skip
+    say "I like your enthusiasm!".upcase
+  	sleep_n_skip
+  	say "The unknown figure is much closer now".upcase
+  	sleep_n_skip
+  	say "You can see clearly that the figure is a giant turtle".upcase
+  	sleep_n_skip
+  	say "Do you: jump on it, jump over it, stop and ask for directions?".upcase
+  	attack = gets.chomp.upcase
+    case attack
+    when "jump on it".upcase
+    	sleep_n_skip
+    	clear_page
+      say bad_choice
+    	sleep_n_skip
+    	say "In the state of Florida, jumping on a sea turtle is a felony!".upcase
+    	sleep_n_skip
+    	say "You are arrested and placed in the county jail with a $2,000.00 bail!".upcase
+    	sleep_n_skip
+    	say "While in jail awful and dehumanizing things happened to you!".upcase
+    	sleep_n_skip
+      say "Better luck next time!".upcase
+    	sleep_skip_game_over
+      end_option
+
+    when "jump over it".upcase  #RUN, JUMP OVER IT
+    	clear_page
+      sleep_n_skip
+      say good_call
+      sleep_n_skip 
+      say "Violence is never the answer!".upcase
+    	sleep_n_skip
+      say "You run up a couple of random brick hills!".upcase
+      sleep_n_skip
+      say "Jump off a giant cliff and even manage to slide down a flagpole!".upcase
+      sleep_n_skip
+      say "Not bad for a plummer!".upcase
+      sleep_n_skip
+      say "All of this happens before an arbitrary 3 minute timer expires!".upcase
+    	sleep_n_skip
+    	say "Unfortunately for you, the princess is in another castle!".upcase
+    	sleep_n_skip
+    	say "Seriously, you should have seen this coming!".upcase
+    	sleep_n_skip
+    	say "Next time consider just starting at World 8 and save yourself about 5 hours!".upcase
+    	sleep_skip_continued
+      end_option
+
+    when "stop and ask for directions".upcase #RUN, STOP AND ASK FOR DIRECTIONS
+      sleep_n_skip
+      clear_page
+      say bad_choice
+      sleep_n_skip
+      say "What were you thinking? Turtles can't talk?!".upcase
+    	sleep_n_skip
+   		say "The turtle bites your big toe and now you can no longer run!".upcase
+   		sleep_n_skip
+   		say "Because of your slow pace, awful and dehumanizing things have happened to the princess!".upcase
+    	sleep_n_skip
+    	say "Maybe we should have sent Chuck Norris?".upcase
+    	clear_page
+      sleep_skip_game_over
+      end_option
+
+    else  #RUN, ANYTHING ELSE
+      sleep_n_skip
+      clear_page
+      say bad_choice
+      sleep_n_skip
+      say "#{attack}? What the hell was that?".upcase
+     	sleep_n_skip
+      say "You have the attention span of a gold fish!".upcase
+      sleep_n_skip
+     	say "Your inability to follow simple instructions will eventually cost you more than this game!".upcase
+     	sleep_n_skip
+    	say "Next time either buy a dictionary or follow the directions a little better!".upcase
+   		sleep_n_skip
+   		say "This princess is doomed!".upcase
+   		clear_page
+      sleep_skip_game_over
+      end_option 	
+    end
+
+  when "do nothing".upcase  #DO NOTHING
+    sleep_n_skip
+    clear_page
+    say bad_choice
+    sleep_n_skip
+    say "Your lack of motivation will cost you!".upcase
+    sleep_n_skip
+    say "Maybe we should have chosen your taller, thinner, and slightly more attractive sibling?".upcase
+    sleep_n_skip
+    say "This princess never had a chance!".upcase
+    sleep_n_skip
+    say "Next time I'm sending a NINJA!".upcase
+    sleep_n_skip
+    clear_page
+    sleep_skip_game_over
+    end_option
+
+	else   #ANYTHING ELSE
+    sleep_n_skip
+    clear_page
+    say bad_choice
+    sleep_n_skip
+    say "#{option}? That was not one of the options!".upcase
+   	sleep_n_skip
+   	say "Your spelling is atrocious!".upcase
+    sleep_n_skip
+   	say "Maybe you should go back to coloring in your coloring book?".upcase
+    sleep_n_skip
+    say "I would have been better off sending my niece and she's 3!".upcase
+    sleep_n_skip
+    say "Adios princess!".upcase
+    clear_page
+    sleep_skip_game_over
+    end_option
+end 
